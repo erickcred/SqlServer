d4e3fc46435605a75c15bd31f392842ec28c8333
Ercik Rick <erickcredjes@gmail.com>
2022-06-14 23:44:21 -0300

    Iniciando com ADO

---
+++ AcessoADados/ADO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = @"Data Source=localhost\SQLEXPRESS;Database=erickcred;TrustServerCertificate=true;User Id=sa;Password=123;";
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                
+                connection.Open();
+                using(SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}")   ;
+                    }
+                }
+                connection.Close();
+            }
+        }
+    }
+}
